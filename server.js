9f5e887c8821793ab8c5c10cc96876bb9fcce450
patrik_dovicovic <72455555+patrik-dovicovic@users.noreply.github.com>
2022-03-04 14:07:06 +0100

    sss

---
+++ server.js
@@ -0,0 +1,20 @@
+//const require
+const express = require('express')
+const app = express()
+const path = require('path');
+const cors = require('cors');
+const fs = require('fs');
+const PORT = process.env.PORT || 5000;
+
+
+
+//app
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")))
+app.use(express.json());
+app.listen(80);
+console.log("ok")
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/', 'home.html'));
+    console.log("server is running")
+});
