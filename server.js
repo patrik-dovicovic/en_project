4097846e1b9054a53507e962ea35a3b914cd8d59
patrik_dovicovic <72455555+patrik-dovicovic@users.noreply.github.com>
2022-03-08 22:34:21 +0100

    d

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const fs = require('fs');
 const PORT = process.env.PORT || 5000;
 
 
-
 //app
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")))
@@ -18,3 +17,16 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/', 'home.html'));
     console.log("server is running")
 });
+app.get('/falconry', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/', 'falconry.html'));
+    console.log("server is running")
+});
+app.get('/windows', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/', 'windows.html'));
+    console.log("server is running")
+});
+app.get('/website', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/', 'website.html'));
+    console.log("server is running")
+});
+
